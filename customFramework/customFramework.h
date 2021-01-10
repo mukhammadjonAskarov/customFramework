4c55bb428e5ecf311b1e3a08f29c64f2c2fb6500
mukhammadjonAskarov <asqarov.muhammadjon@mail.ru>
2021-01-10 11:48:06 +0500

    Initial Commit

---
+++ customFramework/customFramework.h
@@ -0,0 +1,18 @@
+//
+//  customFramework.h
+//  customFramework
+//
+//  Created by Mukhammadjon Askarov on 10/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for customFramework.
+FOUNDATION_EXPORT double customFrameworkVersionNumber;
+
+//! Project version string for customFramework.
+FOUNDATION_EXPORT const unsigned char customFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <customFramework/PublicHeader.h>
+
+
