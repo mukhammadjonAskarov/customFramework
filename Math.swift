167c2ad13d3c64c9c0c692f2574011b3405d6744
mukhammadjonAskarov <asqarov.muhammadjon@mail.ru>
2021-01-10 12:10:57 +0500

    ädd framework

---
+++ Math.swift
@@ -0,0 +1,19 @@
+//
+//  Math.swift
+//  customFramework
+//
+//  Created by Mukhammadjon Askarov on 10/01/21.
+//
+
+import Foundation
+
+struct Math {
+    
+    static func addTwoNumbers(addend1: Double, addend2: Double) -> Double {
+        return addend1  + addend2
+    }
+    
+    static func divideNumbers(dividend: Double, divisor: Double) -> Double{
+        return dividend/divisor
+    }
+}
---
+++ User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  customFramework
+//
+//  Created by Mukhammadjon Askarov on 10/01/21.
+//
+
+import Foundation
+
+
+struct User {
+
+    static func getUserDetails()-> String {
+
+        let user = "User: Mukhammadjon Askarov Contact: +998 98 3377300"
+        return user
+    }
+    
+}
